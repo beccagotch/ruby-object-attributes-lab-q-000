9d60f95717cafa68047cbee44be6e18349e4055c
beccagotch <becca.gotch@archiv8.com>
2016-05-08 19:17:26 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+ #get
+  def name=(dog_name)
+    @name = dog_name
+  end
+#set
+  def name
+    @name
+  end
+
+
+#get
+def breed=(dog_breed)
+  @breed = dog_breed
+end
+#set
+def breed
+  @breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+#get
+def name=(person_name)
+  @name = person_name
+end
+#set
+def name
+  @name
+end
+#get
+def job=(person_job)
+  @job = person_job
+end
+#set
+def job
+  @job
+  end
+end
